d327a569ce186b1ae47f58a3c032cf0fffe892c9
Rafał Krawczyk <rafal.krawczyk@microsoft.wsei.edu.pl>
2022-06-17 03:30:11 +0200

    Dodalem opcje dodawania produktu oraz nawigacje

--- ProjSem_Sklep/Views/Product/AddNewProductPage.xaml.cs
+++ ProjSem_Sklep/Views/Product/AddNewProductPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjSem_Sklep_Lib.Repositories;
+using System;
+using EFProduct = ProjSem_Sklep_Lib.Models.Product;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -18,9 +20,38 @@ namespace ProjSem_Sklep.Views.Product
     /// </summary>
     public partial class AddNewProductPage : Page
     {
-        public AddNewProductPage()
+        private MainWindow _mainWin;
+        private ProductWindow _prodWin;
+        private RepositoryHolder _repoHolder;
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public AddNewProductPage(MainWindow mainWin, ProductWindow prodWin, RepositoryHolder repoHolder)
         {
+            _mainWin = mainWin;
+            _repoHolder = repoHolder;
+            _prodWin = prodWin;
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private void Zapisz_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var nowyProdukt = new EFProduct() { Name = this.Name, Quantity = this.Quantity, Price = this.Price };
+            _repoHolder.ProdRepo.Add(nowyProdukt);
+            _repoHolder.ProdRepo.Save();
+            DataContext = _mainWin;
+            _prodWin.Close();
+        }
+
+        private void Anuluj_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = _mainWin;
+            _prodWin.Close();
         }
     }
 }
